f98f932d4488fb7f206d0935f6e0a8506001e0f1
Corey Gallagher <gallacor@oregonstate.edu>
2022-11-01 14:21:05 -0400

    refactor controller file

--- kaizen-backend/controller/controller.js
+++ kaizen-backend/controller/controller.js
@@ -2,7 +2,7 @@ const Task = require('../models/task.js')
 const User = require('../models/user.js')
 const Project = require('../models/project.js')
 
-const createProject = async (project_name, project_type, project_owner) => {
+async function createProject (project_name, project_type, project_owner){
     // Call the constructor to create an instance of the model class Exercise
 const new_project = await Project.create({
  project_name: project_name,
@@ -11,16 +11,16 @@ const new_project = await Project.create({
 });
 }
 
-const createUser = async (first_name, last_name, email) => {
+async function createUser (first_name, last_name, email) {
     const new_user = await User.create({
         first_name: first_name,
         last_name: last_name,
         email: email
        });
-       comsole.log(new_user)
+       console.log(new_user)
     }
 
-const createTask = async (task_name, task_type, task_owner, task_descriptions, task_status) => {
+async function createTask (task_name, task_type, task_owner, task_descriptions, task_status){
     // Call the constructor to create an instance of the model class Exercise
 const new_task = await Task.create({
  task_name: task_name,
@@ -31,20 +31,21 @@ const new_task = await Task.create({
 });
 }
 
- const deleteTask = async (task_id) => {
+ async function deleteTask (task_id) {
     const deleted_task = await Task.destroy({task_id: task_id})
     console.log(deleted_task)
 }
 
-const deleteProject = async (project_id) => {
-    const deleted_project = await Task.destroy({project_id: project_id})
+async function deleteProject(project_id) {
+    const deleted_project = await Project.destroy({project_id: project_id})
     console.log(deleted_project)
 }
 
-const deleteUser = async (user_id) => {
-    const deleted_user = await Task.destroy({user_id: user_id})
+async function deleteUser (user_id ){
+    const deleted_user = await User.destroy({user_id: user_id})
     console.log(deleted_user)
 }
+
 
 exports.createProject = createProject;
 exports.createTask = createTask;
