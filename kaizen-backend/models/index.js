3be7e4e2e03bba14d5bdd15c89caa05a29e1d437
Corey Gallagher <gallacor@oregonstate.edu>
2022-11-01 13:31:53 -0400

    rename folder to kaizen-backend and change comments

---
+++ kaizen-backend/models/index.js
@@ -0,0 +1,37 @@
+
+const Sequelize = require("sequelize");
+require("dotenv").config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'sqlite',
+  storage: 'data/dev-db.sqlite3'
+});
+
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+// Initializes each table
+db.user = require("./user.js")(sequelize, Sequelize);
+db.project = require("./project.js")(sequelize, Sequelize);
+db.task = require("./task.js")(sequelize, Sequelize);
+
+// M:M project and user
+db.project.belongsToMany(db.user, {
+  through: "projectUser",
+  foreignKey: "user_id"
+});
+db.user.belongsToMany(db.project, {
+  through: "projectUser",
+  foreignKey: "project_id"
+});
+// O:M project to task
+db.task.belongsTo(db.project);
+
+db.project.hasMany(db.task, {
+  as: "tasks"
+});
+
+
+module.exports = db;
