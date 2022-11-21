a84ac3ea00efde2ddb3936409ab7afffb1c9dbdf
fusakoo <obatafusako@gmail.com>
2022-11-20 23:20:51 -0800

    Remove the unused firebase code as that's no longer used

--- src/index.js
+++ src/index.js
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom'
-import { initializeApp } from "firebase/app"
-// Web app's Firebase configuration
-import { firebaseConfig } from './utils/firebase'
-// Initialize Firebase
-initializeApp(firebaseConfig)
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
